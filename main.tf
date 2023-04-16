72a45bdcbe31695e5b30a36bb996cd92d68358ac
Meloudy Yodico <125516086+mldyydcece@users.noreply.github.com>
2023-04-16 11:06:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,82 +1,107 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "REPLACE_ME"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-west-2"
+  region = "eu-west-1"
 }
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+#############################################################
+# Data sources to get VPC and default security group details
+#############################################################
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+###########################
+# Security groups examples
+###########################
+
+#######
+# HTTP
+#######
+module "http_sg" {
+  source = "../../modules/http-80"
+
+  name        = "http-sg"
+  description = "Security group with HTTP ports open for everybody (IPv4 CIDR), egress ports are all world open"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+#####################
+# HTTP with MySQL #1
+#####################
+module "http_mysql_1_sg" {
+  source = "../../modules/http-80"
+
+  name            = "http-mysql-1"
+  use_name_prefix = false
+
+  description = "Security group with HTTP and MySQL ports open for everybody (IPv4 CIDR)"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  # Add MySQL rules
+  ingress_rules = ["mysql-tcp"]
+}
+
+#####################
+# HTTP with MySQL #2
+#####################
+module "http_mysql_2_sg" {
+  source = "../../modules/http-80"
+
+  name        = "http-mysql-2"
+  description = "Security group with HTTP and MySQL ports open within current VPC"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Add mysql rules
+  ingress_rules = ["mysql-tcp"]
+
+  # Allow ingress rules to be accessed only within current VPC
+  ingress_cidr_blocks      = [data.aws_vpc.default.cidr_block]
+  ingress_ipv6_cidr_blocks = [] # Not all VPCs have IPv6 enabled, but if you have it enabled, then this will work - ["${data.aws_vpc.default.ipv6_cidr_block}"]
+}
+
+###########################
+# HTTP with egress minimal
+###########################
+module "http_with_egress_minimal_sg" {
+  source = "../../modules/http-80"
+
+  name        = "http-with-egress-minimal"
+  description = "Security group with HTTP ports open within current VPC, and allow egress access to HTTP ports to the whole world"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Allow ingress rules to be accessed only within current VPC
+  ingress_cidr_blocks = [data.aws_vpc.default.cidr_block]
+
+  # Allow all rules for all protocols
+  egress_rules = ["http-80-tcp"]
+}
+
+###########################
+# HTTP with egress limited
+###########################
+module "http_with_egress_sg" {
+  source = "../../modules/http-80"
+
+  name        = "http-with-egress"
+  description = "Security group with HTTP ports open within current VPC, and allow egress access just to small subnet"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Add mysql rules
+  ingress_rules = ["mysql-tcp"]
+
+  # Allow ingress rules to be accessed only within current VPC
+  ingress_cidr_blocks      = [data.aws_vpc.default.cidr_block]
+  ingress_ipv6_cidr_blocks = [] # Not all VPCs have IPv6 enabled, but if you have it enabled, then this will work - ["${data.aws_vpc.default.ipv6_cidr_block}"]
+
+  # Allow egress rules to access anything (empty list means everything)
+  egress_cidr_blocks      = ["10.10.10.0/28"]
+  egress_ipv6_cidr_blocks = [] # Not all VPCs have IPv6 enabled, but if you have it enabled, then this will work - ["${data.aws_vpc.default.ipv6_cidr_block}"]
 }
